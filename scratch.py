6db376e490c2534ee255be309188a3a9358956b4
BjReeves07 <bjreeves07@gmail.com>
2026-05-31 14:42:57 -0500

    M4: bug hunt notes and test

---
+++ scratch.py
@@ -0,0 +1,4 @@
+﻿def average_rating(ratings):
+    if not ratings:
+        return 0.0
+    return sum(ratings) / len(ratings)
---
+++ tests/test_m4_bughunt.py
@@ -0,0 +1,7 @@
+﻿from scratch import average_rating
+
+def test_empty_ratings_returns_zero():
+    assert average_rating([]) == 0.0
+
+def test_normal_ratings():
+    assert average_rating([4, 5, 3]) == 4.0
